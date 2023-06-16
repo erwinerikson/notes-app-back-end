5ea4874ddd4d1e0457cc90341fe5a1db52b08e2b
Erwin Erikson <erwin_apm@yahoo.co.id>
2023-06-16 18:38:52 +0700

    collaboration implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -18,7 +18,9 @@ class NotesHandler {
             const { title = 'untitled', body, tags } = request.payload;
             const { id: credentialId } = request.auth.credentials;
 
-            const noteId = await this._service.addNote({ title, body, tags, owner: credentialId, });
+            const noteId = await this._service.addNote({
+                title, body, tags, owner: credentialId,
+            });
 
             const response = h.response({
                 status: 'success',
@@ -65,7 +67,7 @@ class NotesHandler {
         try {
             const { id } = request.params;
             const { id: credentialId } = request.auth.credentials;
-            await this._service.verifyNoteOwner(id, credentialId);
+            await this._service.verifyNoteAccess(id, credentialId);
             const note = await this._service.getNoteById(id);
             return {
                 status: 'success',
@@ -100,7 +102,7 @@ class NotesHandler {
             const { id } = request.params;
             const { id: credentialId} = request.auth.credentials;
 
-            await this._service.verifyNoteOwner(id, credentialId);
+            await this._service.verifyNoteAccess(id, credentialId);
             await this._service.editNoteById(id, request.payload);
 
             return {
